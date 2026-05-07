ec2e94ca38ed8909dffcb5dc09827e37a0b8cc7a
AisByte <aiswaryamurugesanofficial@gmail.com>
2026-05-07 23:22:19 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        # dup_dict = {f'{j}':0 for j in nums}
+        # for i in nums:
+        #     dup_dict[f'{i}'] = dup_dict[f'{i}'] + 1
+
+        # dup_list = [True for j in dup_dict if dup_dict[j] > 1]
+
+        # # check if there is True available
+        # return any(dup_list)
+
+        return len(set(nums)) < len(nums)
+
+        
+
